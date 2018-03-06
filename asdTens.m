bd616c5babc50ad535103c9bec7e7eb8c0ee3bcf
adamsandler <adamsandler@dhcp-10-105-148-119.wireless.northwestern.private>
2018-03-05 18:05:25 -0600

    modified compile script

--- asdTens.m
+++ asdTens.m
@@ -2,7 +2,7 @@ asdSparse=csvread('asdSparse.csv',1,1);
 asd=sptensor(asdSparse(:,1:3),asdSparse(:,4));
 % asd=poissrnd(2,20,20,20);
 % asd=sptensor(asd);
-mex drawZscPar.c;
+mex -v drawZscPar.c CFLAGS="$CFLAGS -fopenmp" LDFLAGS="$LDFLAGS -fopenmp";
 %[tuck, tree]=asdHBTucker(asd,2,0.5);
 tic;
 [phi, psi, tree]=asdHBTucker2(asd,2,0.5);
