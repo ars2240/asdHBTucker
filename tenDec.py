83c178129aa8de667a82138215b3c1b33bcdcdac
ars2240 <ars2240@columbia.edu>
2020-03-13 14:07:31 -0500

    tweak

--- tenDec.py
+++ tenDec.py
@@ -14,6 +14,7 @@ import pandas as pd
 import sparse
 from tensorly.contrib.sparse import tensor
 from tensorly.contrib.sparse.decomposition import parafac
+import time
 
 
 def ten_dec(fname='cancerSparse', indF='cancerCVInd', rank=5, fselect='min dupe', fmin=0, fmax=1000, thresh=0,
@@ -38,17 +39,21 @@ def ten_dec(fname='cancerSparse', indF='cancerCVInd', rank=5, fselect='min dupe'
         if 'min' in fselect:
             # remove slices with min or fewer occurances
             cols = (X.astype(bool).sum(axis=(0, 2)) > fmin).todense()
+            #print(np.where((X.astype(bool).sum(axis=(0, 2)) <= fmin).todense())[0])
             X = X[:, cols, :]
             Xv = Xv[:, cols, :]
             cols = (X.astype(bool).sum(axis=(0, 1)) > fmin).todense()
+            #print(np.where((X.astype(bool).sum(axis=(0, 1)) <= fmin).todense())[0])
             X = X[:, :, cols]
             Xv = Xv[:, :, cols]
         if 'max' in fselect:
             # remove slices with max or more occurances
             cols = (X.astype(bool).sum(axis=(0, 2)) < fmax).todense()
+            #print((X.astype(bool).sum(axis=(0, 2)) >= fmin).todense())
             X = X[:, cols, :]
             Xv = Xv[:, cols, :]
             cols = (X.astype(bool).sum(axis=(0, 1)) < fmax).todense()
+            #print((X.astype(bool).sum(axis=(0, 1)) >= fmin).todense())
             X = X[:, :, cols]
             Xv = Xv[:, :, cols]
         if 'thresh' in fselect:
@@ -58,24 +63,66 @@ def ten_dec(fname='cancerSparse', indF='cancerCVInd', rank=5, fselect='min dupe'
         if 'dupe' in fselect:
             s = X.shape
             # check for and remove duplicate slices
+            dupes = [8, 10, 24, 30, 32, 174, 205, 206, 237, 240, 241, 245, 248, 251, 303, 311, 318, 355, 360, 392, 412,
+                     448, 499, 595, 640, 642, 647, 662, 684, 711, 729, 741, 962, 972, 985, 992, 993, 994, 995, 996,
+                     1009, 1012, 1041, 1057, 1095, 1133, 1134, 1135, 1169, 1179, 1181, 1182, 1187, 1188, 1217, 1233,
+                     1239, 1285, 1286, 1299, 1321, 1342, 1346, 1351, 1356, 1406, 1408, 1413, 1425, 1438, 1441, 1445,
+                     1446, 1455, 1490, 1557, 1565, 1651, 1653, 1675]
+            """
+            for j in range(1, s[2]):
+                for k in range(0, j):
+                    #start = time.time()
+                    if np.abs(X[:, :, j]-X[:, :, k]).sum() == 0:
+                        print('%d, %d' % (j, k))
+                        dupes.append(j)
+                        break
+                    #end = time.time()
+                    #t = end - start
+                    #print(t)
+            print(dupes)
+            """
+            #print(len(dupes))
+            cols = list(set(range(0, s[2])).difference(set(dupes)))
+            X = X[:, :, cols]
+            Xv = Xv[:, :, cols]
+            """
             dupes = []
             for j in range(1, s[1]):
                 for k in range(0, j):
-                    if np.array_equal(X[:, j, :].todense(), X[:, k, :].todense()):
+                    if np.abs(X[:, j, :]-X[:, k, :]).sum() == 0:
+                        print('%d, %d' % (j, k))
                         dupes.append(j)
                         break
             print(dupes)
-            dupes = []
-            for j in range(1, s[2]):
-                for k in range(0, j):
-                    if np.array_equal(X[:, :, j].todense(), X[:, :, k].todense()):
-                        dupes.append(j)
-                        break
-            print(dupes)
+            cols = list(set(range(0, s[1])).difference(set(dupes)))
+            X = X[:, cols, :]
+            Xv = Xv[:, cols, :]
+            """
+        fmin = 200
+        # remove slices with min or fewer occurances
+        cols = (X.astype(bool).sum(axis=(0, 2)) > fmin).todense()
+        #print(np.where((X.astype(bool).sum(axis=(0, 2)) <= fmin).todense())[0])
+        X = X[:, cols, :]
+        Xv = Xv[:, cols, :]
+        cols = (X.astype(bool).sum(axis=(0, 1)) > fmin).todense()
+        #print(np.where((X.astype(bool).sum(axis=(0, 1)) <= fmin).todense())[0])
+        X = X[:, :, cols]
+        Xv = Xv[:, :, cols]
+        fmax = 1000
+        # remove slices with max or more occurances
+        cols = (X.astype(bool).sum(axis=(0, 2)) < fmax).todense()
+        # print((X.astype(bool).sum(axis=(0, 2)) >= fmin).todense())
+        X = X[:, cols, :]
+        Xv = Xv[:, cols, :]
+        cols = (X.astype(bool).sum(axis=(0, 1)) < fmax).todense()
+        # print((X.astype(bool).sum(axis=(0, 1)) >= fmin).todense())
+        X = X[:, :, cols]
+        Xv = Xv[:, :, cols]
+        print(X.shape)
 
         phiT = tensor(X)
         factors = parafac(phiT, rank=rank, init='random')
         print(factors)
 
 
-ten_dec(fmin=5)
+ten_dec(fselect='min max dupe', fmin=200, fmax=1000)
